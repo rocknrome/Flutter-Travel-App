dbfd5c3b94cf44130afa74bd530d98866c70008d
Roman Larionov <pcsaveddotcom@gmail.com>
2024-03-16 23:44:55 -0400

    2-models created

--- lib/models/destination_model.dart
+++ lib/models/destination_model.dart
@@ -1,17 +1,62 @@
 import 'package:flutter/material.dart';
 
-class Destination extends StatelessWidget {
-  const Destination({Key? key}) : super(key: key);
+class Destination {
+  final String imageUrl;
+  final String city;
+  final String country;
+  final String description;
+  final List<Activity> activities;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Travel App'),
-      ),
-      body: const Center(
-        child: Text('Welcome to the Travel App!'),
-      ),
-    );
-  }
+  Destination({
+    required this.imageUrl,
+    required this.city,
+    required this.country,
+    required this.description,
+    required this.activities,
+  });
 }
+
+class Activity {
+  final String imageUrl;
+  final String name;
+  final String type;
+  final List<String> startTimes;
+  final int rating;
+  final int price;
+
+  Activity({
+    required this.imageUrl,
+    required this.name,
+    required this.type,
+    required this.startTimes,
+    required this.rating,
+    required this.price,
+  });
+}
+
+List<Activity> activities = [
+  Activity(
+    imageUrl: 'assets/images/stmarksbasilica.jpg',
+    name: 'St. Mark\'s Basilica',
+    type: 'Sightseeing Tour',
+    startTimes: ['9:00 am', '11:00 am'],
+    rating: 5,
+    price: 30,
+  ),
+  Activity(
+    imageUrl: 'assets/images/gondola.jpg',
+    name: 'Walking Tour and Gondola Ride',
+    type: 'Sightseeing Tour',
+    startTimes: ['11:00 am', '1:00 pm'],
+    rating: 4,
+    price: 210,
+  ),
+  Activity(
+    imageUrl: 'assets/images/murano.jpg',
+    name: 'Murano and Burano Tour',
+    type: 'Sightseeing Tour',
+    startTimes: ['12:00 pm', '2:00 pm'],
+    rating: 3,
+    price: 125,
+  ),
+];
