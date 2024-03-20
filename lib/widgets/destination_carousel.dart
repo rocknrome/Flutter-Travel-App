0d9ba8c05be6e9090103540ed209a2cb75013d6f
Roman Larionov <pcsaveddotcom@gmail.com>
2024-03-20 19:40:41 -0400

    18-destination carousel working. No Problems. Hero and GestureDetetor implemented

--- lib/widgets/destination_carousel.dart
+++ lib/widgets/destination_carousel.dart
@@ -25,13 +25,12 @@ class DestinationCarousel extends StatelessWidget {
               ),
               GestureDetector(
                 onTap: () {
-                  // ignore: avoid_print
-                  print("See All"); // FOR TESTING PURPOSES
+                  // Future implementation for "See All" action
                 },
                 child: Text(
                   'See All',
                   style: TextStyle(
-                    color: Theme.of(context).primaryColor,
+                    color: Theme.of(context).colorScheme.secondary,
                     fontSize: 16.0,
                     fontWeight: FontWeight.w600,
                     letterSpacing: 1.0,
@@ -48,128 +47,126 @@ class DestinationCarousel extends StatelessWidget {
             itemCount: destinations.length,
             itemBuilder: (BuildContext context, int index) {
               Destination destination = destinations[index];
-              return Container(
-                color: Colors.red,
-                width: 210.0,
-                margin: const EdgeInsets.all(10.0),
-                child: Stack(
-                  alignment: Alignment.topCenter,
-                  children: <Widget>[
-                    Positioned(
-                      bottom: 10.0,
-                      child: Container(
-                        height:
-                            120.0, // Fixed height Container to wrap the description for consistent look of cards' description
-                        width: 200.0,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                '${destination.activities.length} activities',
-                                style: const TextStyle(
-                                  fontSize: 22.0,
-                                  fontWeight: FontWeight.w600,
-                                  letterSpacing: 1.2,
-                                ),
-                                maxLines: 1, // Ensures this text does not wrap
-                                overflow: TextOverflow
-                                    .ellipsis, // Adds ellipsis if text overflows
-                              ),
-                              SizedBox(
-                                height:
-                                    45.0, // Fixed height for the description text container
-                                child: Text(
-                                  destination.description,
+              return GestureDetector(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => DestinationScreen(
+                      destination: destination,
+                    ),
+                  ),
+                ),
+                child: Container(
+                  width: 210.0,
+                  margin: const EdgeInsets.all(10.0),
+                  child: Stack(
+                    alignment: Alignment.topCenter,
+                    children: <Widget>[
+                      Positioned(
+                        bottom: 10.0,
+                        child: Container(
+                          height: 120.0,
+                          width: 200.0,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  '${destination.activities.length} activities',
                                   style: const TextStyle(
-                                    color: Colors.grey,
+                                    fontSize: 22.0,
+                                    fontWeight: FontWeight.w600,
+                                    letterSpacing: 1.2,
                                   ),
+                                  maxLines: 1,
                                   overflow: TextOverflow.ellipsis,
-                                  maxLines:
-                                      3, // Allows for up to 3 lines of text
                                 ),
+                                SizedBox(
+                                  height: 45.0,
+                                  child: Text(
+                                    destination.description,
+                                    style: const TextStyle(
+                                      color: Colors.grey,
+                                    ),
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 3,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Hero(
+                        tag: destination.imageUrl,
+                        child: Container(
+                          height: 190.0,
+                          width: 190.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20.0),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors.black26,
+                                offset: Offset(0.0, 2.0),
+                                blurRadius: 6.0,
                               ),
                             ],
                           ),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      height: 190.0,
-                      width: 190.0,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20.0),
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Colors.black26,
-                            offset: Offset(0.0, 2.0),
-                            blurRadius: 6.0,
-                          ),
-                        ],
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20.0),
-                        child: GestureDetector(
-                          onTap: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (_) => DestinationScreen(
-                                destination: destination,
-                              ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20.0),
+                            child: Image(
+                              image: AssetImage(destination.imageUrl),
+                              fit: BoxFit.cover,
                             ),
-                          ),
-                          child: Image(
-                            image: AssetImage(destination.imageUrl),
-                            fit: BoxFit.cover,
                           ),
                         ),
                       ),
-                    ),
-                    Positioned(
-                      left: 20.0,
-                      bottom: 120.0,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            destination.city,
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 24.0,
-                              fontWeight: FontWeight.w600,
-                              letterSpacing: 1.2,
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              const Icon(
-                                FontAwesomeIcons.locationArrow,
-                                size: 10.0,
+                      Positioned(
+                        left: 20.0,
+                        bottom: 120.0,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              destination.city,
+                              style: const TextStyle(
                                 color: Colors.white,
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.w600,
+                                letterSpacing: 1.2,
                               ),
-                              const SizedBox(width: 5.0),
-                              Text(
-                                destination.country,
-                                style: const TextStyle(
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                const Icon(
+                                  FontAwesomeIcons.locationArrow,
+                                  size: 10.0,
                                   color: Colors.white,
-                                  fontSize: 20.0,
-                                  fontWeight: FontWeight.w500,
-                                  letterSpacing: 1.2,
                                 ),
-                              ),
-                            ],
-                          ),
-                        ],
+                                const SizedBox(width: 5.0),
+                                Text(
+                                  destination.country,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w500,
+                                    letterSpacing: 1.2,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               );
             },
